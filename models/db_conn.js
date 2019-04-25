4d67af72710d2070d26424a61a6be2b2f33e9e91
meowtsai <shihfan.tsai@gmail.com>
2019-04-25 14:04:02 +0800

    for first lanuch

--- models/db_conn.js
+++ models/db_conn.js
@@ -3,7 +3,7 @@ const CONFIG = require("../config/config")[process.env.NODE_ENV];
 
 // create the connection to database
 const pool = mysql.createPool({
-  host: CONFIG.db_host2,
+  host: CONFIG.db_host1,
   user: CONFIG.db_user,
   database: CONFIG.db_database,
   password: CONFIG.db_password,
