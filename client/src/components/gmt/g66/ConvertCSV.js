2108045da82f551376ce981203073e974c1432c2
meowtsai <shihfan.tsai@gmail.com>
2019-04-25 16:01:36 +0800

    add download csv npm

--- client/src/components/gmt/g66/ConvertCSV.js
+++ client/src/components/gmt/g66/ConvertCSV.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { CSVLink, CSVDownload } from "react-csv";
+
 import InputGroup from "../../common/InputGroup";
 import { getCSV } from "../../../actions/gmtActions";
+
 class ConvertCSV extends Component {
   state = {
     url: ""
@@ -23,59 +26,69 @@ class ConvertCSV extends Component {
     const { csv } = this.props.gmt;
     const { errors } = this.props;
     //************************** */
-    var lines = csv.split("\n");
-    let colIndex = 0;
     let tableHeader;
 
     let tableRow = [];
     let item_attr_str;
-    for (var line = 0; line < lines.length; line++) {
-      // By tabs
-      //console.log(line);
-      var tabs = lines[line].split("\t");
-      for (var tab = 0; tab < tabs.length; tab++) {
-        //alert(tabs[tab]);
-        if (line === 0) {
-          if (tabs[tab] === "item_attr_str") {
-            colIndex = tab;
+    let csvData = [];
+    if (csv.length > 0) {
+      var lines = csv.split("\n");
+      let colIndex = 0;
+
+      for (var line = 0; line < lines.length; line++) {
+        // By tabs
+        //console.log(line);
+        var tabs = lines[line].split("\t");
+        for (var tab = 0; tab < tabs.length; tab++) {
+          //alert(tabs[tab]);
+          if (line === 0) {
+            if (tabs[tab] === "item_attr_str") {
+              colIndex = tab;
+            }
           }
         }
-      }
-      if (line === 0) {
-        tableHeader = tabs.map(tabItem => <th>{tabItem}</th>);
-      } else {
-        //find all item_ids in item_attr_str field
-        if (tabs[colIndex] !== undefined) {
-          item_attr_str = tabs[colIndex].slice(
-            tabs[colIndex].indexOf("={") + 1
-          );
-          let removeIndexStart = item_attr_str
-            .substring(0, item_attr_str.lastIndexOf("}"))
-            .lastIndexOf("}");
-          //item_attr_str = tabs[colIndex].replace("{equip=", "");
-          //let removeIndexStart = item_attr_str.lastIndexOf(",");
-          item_attr_str = item_attr_str.substring(0, removeIndexStart + 1);
-          let allItems = [];
-          try {
-            let itemObj = JSON.parse(item_attr_str);
 
-            allItems.push(itemObj.item_id);
-            if (itemObj.modules) {
-              for (const [key, value] of Object.entries(itemObj.modules)) {
-                //console.log(key, value);
-                if (value.item_id) {
-                  allItems.push(value.item_id);
+        if (line === 0) {
+          console.log(tabs);
+          tabs.push("extracted item_ids");
+          csvData.push(tabs);
+
+          tableHeader = tabs.map(tabItem => <th>{tabItem}</th>);
+        } else {
+          //find all item_ids in item_attr_str field
+          if (tabs[colIndex] !== undefined) {
+            item_attr_str = tabs[colIndex].slice(
+              tabs[colIndex].indexOf("={") + 1
+            );
+            let removeIndexStart = item_attr_str
+              .substring(0, item_attr_str.lastIndexOf("}"))
+              .lastIndexOf("}");
+            //item_attr_str = tabs[colIndex].replace("{equip=", "");
+            //let removeIndexStart = item_attr_str.lastIndexOf(",");
+            item_attr_str = item_attr_str.substring(0, removeIndexStart + 1);
+            let allItems = [];
+            try {
+              let itemObj = JSON.parse(item_attr_str);
+
+              allItems.push(itemObj.item_id);
+              if (itemObj.modules) {
+                for (const [key, value] of Object.entries(itemObj.modules)) {
+                  //console.log(key, value);
+                  if (value.item_id) {
+                    allItems.push(value.item_id);
+                  }
                 }
               }
+            } catch (e) {
+              console.log(`line${item_attr_str}`, e.message);
             }
-          } catch (e) {
-            console.log(`line${item_attr_str}`, e.message);
+
+            tabs.push(`\'${allItems.join(",")}`);
           }
 
-          tabs.push(allItems.join(","));
+          csvData.push(tabs);
+          tableRow.push(tabs.map(tabItem => <td>{tabItem}</td>));
         }
-
-        tableRow.push(tabs.map(tabItem => <td>{tabItem}</td>));
       }
 
       //console.log(item_attr_str);
@@ -106,7 +119,16 @@ class ConvertCSV extends Component {
           </div>
         </div>
         <div>
-          <table className="table table-bordered table-sm">
+          {csvData.length > 0 && (
+            <CSVLink data={csvData} separator={"\t"}>
+              下載 csv檔案
+            </CSVLink>
+          )}
+
+          <table
+            className="table table-bordered table-sm"
+            style={{ fontSize: "9pt" }}
+          >
             <thead>
               <tr>{tableHeader}</tr>
             </thead>
